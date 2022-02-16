212820f79fc0dcc6ceefa3b0f5d312162ad8b00a
Rysent <735134025@qq.com>
2022-02-16 23:23:00 +0800

    Learning in Go.

--- Golang/Learning in Go/Gin/Hello World/Hello World.go
+++ Golang/Learning in Go/Gin/Hello World/Hello World.go
@@ -12,5 +12,8 @@ func main() {
 			"message": "Hello World",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err := r.Run()
+	if err != nil {
+		return 
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
