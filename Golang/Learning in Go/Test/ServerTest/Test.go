3f348d00a4736b588fbaae1c63bce1fe6ac479fb
Mazhe <mazhe@wasu.com>
2022-03-16 14:35:32 +0800

    Learning in GO

---
+++ Golang/Learning in Go/Test/ServerTest/Test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world")
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	http.ListenAndServe(":8000", nil)
+
+}
