9e4620ba6b005533cd950b210a1506cb52c2b225
Mazhe <mazhe@wasu.com>
2022-05-16 10:32:02 +0800

    Go & routine

--- Goal.md
+++ Goal.md
@@ -38,3 +38,4 @@ SQL语句
 ![后端](http://www.bitcountrys.com/img/backend.png)
 
 
+https://csdiy.wiki/CS%E5%AD%A6%E4%B9%A0%E8%A7%84%E5%88%92/
--- Golang/Learning in Go/Syntax/Time/TimeTest.go
+++ Golang/Learning in Go/Syntax/Time/TimeTest.go
@@ -1,10 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println(time.Now())
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.POST("", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"1": "123",
+			"2": "333",
+		})
+	})
+	r.Any("/any", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"1": "123",
+			"2": "333",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
